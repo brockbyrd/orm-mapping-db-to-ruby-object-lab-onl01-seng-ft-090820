41d04d418fb37b279ccf9ece47beebe37a20ba6c
Brock Byrd <brockbyrdd@gmail.com>
2020-10-07 18:59:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,7 +78,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade < 12
+      WHERE students.grade < 12
     SQL
 
     DB[:conn].execute(sql).collect do |row|
